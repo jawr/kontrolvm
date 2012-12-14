a38271df085faa0a40191f2cbf675ff79580f18f
jawr <jawr@jarrah>
2012-12-14 20:09:54 +0000

    Aesthetic changes.

--- kontrolvm/settings.py
+++ kontrolvm/settings.py
@@ -89,6 +89,7 @@ MIDDLEWARE_CLASSES = (
   'django.contrib.auth.middleware.AuthenticationMiddleware',
   'django.contrib.messages.middleware.MessageMiddleware',
   'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  'apps.account.middleware.AjaxMessaging',
 )
 
 ROOT_URLCONF = 'kontrolvm.urls'
