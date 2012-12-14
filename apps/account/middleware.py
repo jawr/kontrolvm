78d4f8adec2d6d86eb6d5aa747cc788036f91dbb
jawr <jawr@jarrah>
2012-12-14 17:17:53 +0000

    Added middleware to handle messages on ajax requests.

---
+++ apps/account/middleware.py
@@ -0,0 +1,26 @@
+import simplejson as json
+
+from django.contrib import messages
+
+class AjaxMessaging(object):
+    def process_response(self, request, response):
+        if request.is_ajax():
+            if response['Content-Type'] in ["application/javascript", "application/json"]:
+                try:
+                    content = json.loads(response.content)
+                except ValueError:
+                    return response
+
+                django_messages = []
+
+                for message in messages.get_messages(request):
+                    django_messages.append({
+                        "level": message.level,
+                        "message": message.message,
+                        "extra_tags": message.tags,
+                    })
+
+                content['django_messages'] = django_messages
+
+                response.content = json.dumps(content)
+        return response
