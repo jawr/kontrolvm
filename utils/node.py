82ef02432f253d3415a3f6e26e46fdb83a2ae0a4
jawr <jawr@jarrah>
2012-12-17 17:29:45 +0000

    More robust error checking. Implemented abort command.

--- utils/node.py
+++ utils/node.py
@@ -6,11 +6,22 @@ def send_command(hypervisor, command, args):
   headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
   payload = {'command': command, 'args': args}
   result = requests.post(addr, data=simplejson.dumps(payload), headers=headers)
-
   return result.text
 
 def check_command(hypervisor, task_id):
-  addr = '%s/cmd/%s/' % (hypervisor.node_address, task_id)
+  addr = '%s/cmd/status/%s/' % (hypervisor.node_address, task_id)
   headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
   result = requests.get(addr)
-  return simplejson.loads(result.text)
+  print result.status_code
+  print result.text
+  if result.status_code == 200:
+    return simplejson.loads(result.text)
+  return {'state': 'ERROR'}
+
+def abort_command(hypervisor, task_id):
+  addr = '%s/cmd/abort/%s/' % (hypervisor.node_address, task_id)
+  headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+  result = requests.get(addr)
+  if result.status_code == 200:
+    return simplejson.loads(result.text)
+  return {'state': 'ERROR'}
