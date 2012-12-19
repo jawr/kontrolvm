1c307c4a36c1c6652fa879fe4bc6592e85763e3a
jawr <jawr@jarrah>
2012-12-19 21:44:02 +0000

    Hypervisor initalizer now checks for installation media in given path and creates objects (defaults to user[0] objects, might need to be refactored).

--- apps/hypervisor/tasks.py
+++ apps/hypervisor/tasks.py
@@ -1,10 +1,14 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import User
 from xml.dom import minidom
 from celery import task
-from apps.storagepool.models import Hypervisor
+from apps.hypervisor.models import Hypervisor
 from apps.storagepool.models import StoragePool
+from apps.installationdisk.models import InstallationDisk
+from utils import node
 import libvirt
+import time
 
 @task()
 def initalize_hypervisor(hypervisor):
@@ -23,6 +27,30 @@ def initalize_hypervisor(hypervisor):
         path=path,
         hypervisor=hypervisor
       )
+    # get a list of existing installation disks
+    task_id = node.send_command(hypervisor, 'installationdisk_list', 
+      {'path': hypervisor.install_medium_path})
+    now = time.time()
+    while True:
+      time.sleep(2.5)
+      status = node.check_command(hypervisor, task_id)
+      print status
+      if status['state'] == 'SUCCESS':
+        print status['args']['disks']
+        for disk in status['args']['disks']:
+          print disk
+          (installationdisk, created) = InstallationDisk.objects.get_or_create(
+            name=disk['filename'],
+            hypervisor=hypervisor,
+            url='Unknown',
+            filename=disk['filename'],
+            total_bytes=disk['total_bytes'],
+            user=User.objects.all()[0] # not brilliant..
+          )
+          if created: installationdisk.save()
+        break
+      elif status['state'] == 'FAILED' or status['state'] == 'ERROR' or time.time() - now > 60:
+        break 
 
 @receiver(post_save, sender=Hypervisor)
 def initalize_hypervisor_signal(sender, **kwargs):
