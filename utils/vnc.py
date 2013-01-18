c6da8988a669ad9903824cdde0ba5239ecf429c4
jawr <jawr@jarrah>
2013-01-18 14:39:29 +0000

    Improved threading for Proxies so that we can clean them up correctly.

--- utils/vnc.py
+++ utils/vnc.py
@@ -1,4 +1,4 @@
-from threading import Thread
+from threading import Thread, Event
 import socket
 
 class ProxyPipe(Thread):
@@ -9,10 +9,14 @@ class ProxyPipe(Thread):
     self.source = source
     self.sink = sink
     self.port = port
+    self._stop = Event()
     ProxyPipe.pipes.append(self)
 
+  def stop(self):
+    self._stop.set()
+
   def run(self):
-    while 1:
+    while not self._stop.is_set():
       try:
         data = self.source.recv(1024)
         if not data: break
@@ -29,6 +33,9 @@ class ProxyPipe(Thread):
     except ValueError:
       pass
 
+  def stop(self):
+    self._stop.set()
+
 class Proxy(Thread):
   listeners = []
   def __init__(self, listen_host, listen_port, dest_host, dest_port):
@@ -41,30 +48,20 @@ class Proxy(Thread):
     self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.sock.bind((self.listen_host, self.listen_port))
     self.sock.listen(5) #connection queue
+    self._stop = Event()
     Proxy.listeners.append(self)
 
-  """ This needs to deactivate the Model in the db """
-  def cleanup(self):
-    print 'deleting proxy ' + str(self.dest_port)
-    vnc_gate.close_gate(self.dest_port)
-    for pipe in ProxyPipe.pipes:
-      if pipe.port == self.dest_port:
-        pipe.cleanup()
-        del pipe
-    self.sock.close()
-    try:
-      ProxyPipe.pipes.remove(self)
-    except ValueError:
-      pass
+  def stop(self):
+    self._stop.set()
 
   def run(self):
     print "Running, listening on %s:%d" % (self.listen_host, self.listen_port)
     print "Forwarding to: %s:%d" % (self.dest_host, self.dest_port)
-    while 1:
+    while not self._stop.is_set():
       newsock, address = self.sock.accept()
       forward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       forward.connect((self.dest_host, self.dest_port))
-      print 'got new connection from'
-      print address
-      ProxyPipe(newsock, forward, self.dest_port).start()
-      ProxyPipe(forward, newsock, self.dest_port).start()
+      self.pipe1 = ProxyPipe(newsock, forward, self.dest_port).start()
+      self.pipe2 = ProxyPipe(forward, newsock, self.dest_port).start()
+    if self.pipe1: self.pipe1.stop()
+    if self.pipe2: self.pipe2.stop()
