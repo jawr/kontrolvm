3a09e768b6cb1fe1289d9b8ba00592b864746cdc
jawr <jawr@jarrah>
2013-04-30 15:15:35 +0100

    Django 1.5 fixes

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView                                   
+
 urlpatterns = patterns('apps.account.views',
   url(r'^login', 'account_login'),
   url(r'^logout', 'account_logout'),
   url(r'^$', 'index'),
   url(r'^admin', 'admin'),
   url(r'^add', 'add'),
-  url(r'^success', direct_to_template, {'template': 'account/success.html'}),
+  url(r'^success', TemplateView.as_view(template_name='account/success.html')),
 )
