6635a2d017d6121b17c1cee18e9c10b927291fd7
jawr <jess@lawrence.pm>
2013-05-21 00:29:50 +0100

    Fixed start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -4,5 +4,5 @@ PIDFILE="$PROJECT/kontrolvm.pid"
 if [ ! -f $PIDFILE ]; then
 	cd $PROJECT
 	echo 'starting server'
-	python manage.py runfcgi pidfile=$PIDFILE host=127.0.0.1 port=6060 --settings=settings
+	python manage.py runfcgi pidfile=$PIDFILE host=127.0.0.1 port=6060
 fi
